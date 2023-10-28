7f081e4fab2a207dae9dfd402bf6e156316ba2d4
joao.cordeiro <joao.victor@nubbi.com.br>
2023-10-27 21:13:48 -0300

    Ajuste no console

--- public/js/script.js
+++ public/js/script.js
@@ -11,18 +11,18 @@ const consoleAccess = async () => {
     const data = await reponse.json()
 
     $('#json-access').html('')
-    $('#json-access').append('<pre>' + JSON.stringify(data, null, 2) + '</pre>')   
+    $('#json-access').append('<pre>' + JSON.stringify(data, null, 2) + '</pre>')
 }
 
 setInterval(() => {
-    if (!$('#json-err').is(':hover')){       
-        consoleErr()        
+    if (!$('#json-err').is(':hover')) {
+        consoleErr()
         var objDivErr = document.getElementById("json-err");
-        objDivErr.scrollTop = objDivErr.scrollHeight;        
-    }  
-    if (!$('#json-access').is(':hover')){               
-        consoleAccess()        
-        var objDivAccess = document.getElementById("json-access");              
+        objDivErr.scrollTop = objDivErr.scrollHeight;
+    }
+    if (!$('#json-access').is(':hover')) {
+        consoleAccess()
+        var objDivAccess = document.getElementById("json-access");
         objDivAccess.scrollTop = objDivAccess.scrollHeight;
-    }   
-}, 1000)
+    }
+}, 2000)
